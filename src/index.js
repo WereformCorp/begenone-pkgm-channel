c5def0baf64d326fc9cb66ccf1e6d5459edbd70e
areeshalam <areeshpersonal5@gmail.com>
2025-12-05 01:38:05 +1100

    Saved: Added Simple Channel Wire Layout and Exported it for temporary consumption in the app (main)

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ export { ChannelNavigation } from "./components/ChannelNavigation";
 export { ChannelHomeLayout } from "./layouts/home/ChannelHomeLayout";
 export { ChannelBaseLayout } from "./layouts/ChannelBaseLayout";
 export { ChannelVideoLayout } from "./layouts/videos/ChanneLVideoLayout";
+export { ChannelWireLayout } from "./layouts/wires/ChannelWireLayout";
 export { ChannelAboutLayout } from "./layouts/about/ChannelAboutLayout";
