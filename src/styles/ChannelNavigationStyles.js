8249ce7ca09040bdbae71f43b335fde616cf54d5
areeshalam <areeshpersonal5@gmail.com>
2026-01-18 19:46:58 +1100

    docs(ui): add one-line intent comments to channel navigation and global styles

--- src/styles/ChannelNavigationStyles.js
+++ src/styles/ChannelNavigationStyles.js
@@ -1,39 +1,39 @@
 import { StyleSheet } from "react-native";
 import { globalStyles } from "./globalStyles";
 
+/* Channel tab navigation bar styles */
 export const ChannelNavigationStyles = StyleSheet.create({
   container: {
+    // Navigation bar container
     width: "auto",
     height: 60,
     flexDirection: "row",
     justifyContent: "space-between",
-    // marginTop: 6,
     paddingTop: 18,
     paddingRight: 18,
     paddingLeft: 18,
     paddingBottom: 18,
     marginBottom: 18,
-
     borderBottomWidth: 1,
     borderBottomColor: globalStyles.colors.colorPrimary350,
   },
 
   button: {
+    // Individual navigation button
     width: 80,
     height: 30,
-    // marginRight: 12,
     alignItems: "center",
     justifyContent: "center",
-    // backgroundColor: globalStyles.colors.colorPrimary200,
-    // borderRadius: globalStyles.borders.borderPrimary50,
   },
 
   selectedButton: {
+    // Active navigation state
     backgroundColor: globalStyles.colors.colorPrimary350,
     borderRadius: globalStyles.borders.borderPrimary50,
   },
 
   buttonText: {
+    // Navigation label text
     color: "#fff",
   },
 });
