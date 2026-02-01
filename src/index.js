f3d5d5c3694230231a6a799572b52f3f70a95bab
areeshalam <areeshpersonal5@gmail.com>
2026-02-01 19:56:00 +1100

    Publishing V 1.0.6

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ export { ChannelHeader } from "./components/ChannelHeader";
 export { ChannelNavigation } from "./components/ChannelNavigation";
 export { ChannelHomeLayout } from "./layouts/home/ChannelHomeLayout";
 export { ChannelBaseLayout } from "./layouts/ChannelBaseLayout";
-export { ChannelVideoLayout } from "./layouts/videos/ChanneLVideoLayout";
+export { ChannelVideoLayout } from "./layouts/videos/ChannelVideoLayout";
 export { ChannelWireLayout } from "./layouts/wires/ChannelWireLayout";
 export { ChannelAboutLayout } from "./layouts/about/ChannelAboutLayout";
